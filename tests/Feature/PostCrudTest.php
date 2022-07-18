2bd4f9a24de5e9f6ce94ee6ddb63ec7f0a71cee1
Tanjil Hasan <tanjil@axilweb.com>
2022-07-18 16:51:02 +0600

    post crud test

--- tests/Feature/PostCrudTest.php
+++ tests/Feature/PostCrudTest.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\Response;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Tests\TestCase;
 
@@ -76,9 +77,104 @@ class PostCrudTest extends TestCase
             'title' => 'test title',
             'short_description' => 'lorem ipsum dolor emet sit.',
             'full_details' => 'ha ha ha lorem ipsum dolor emit sit',
-            'image' => new UploadedFile(public_path('img/1.png'), '1.png', null, null, true)
+            'image' => 'new UploadedFile(public_path(\'img/1.png\'), \'1.png\', null, null, true)'
         ];
         $response = $this->actingAs($user)->post(route('post.store'), $params);
-        $response->assertSessionHasErrors();
+        $response->assertSessionHasErrors(['image']);
+
+        $params = [
+            'title' => 'test title',
+            'short_description' => 'lorem ipsum dolor emet sit.',
+            'full_details' => 'ha ha ha lorem ipsum dolor emit sit',
+            'image' => new UploadedFile(public_path('img/1.png'), '1.png', null, null, true)
+        ];
+
+        $this->actingAs($user)
+            ->from(route('post.index'))
+            ->post(route('post.store'), $params)
+            ->assertStatus(302)
+            ->assertRedirect(route('post.index'));
+    }
+
+
+    public function test_update_old_post()
+    {
+        $user = User::factory()->create();
+        Post::factory(10)->create();
+        $post = Post::query()->inRandomOrder()->first();
+
+        $params = [];
+        $response = $this->actingAs($user)->put(route('post.update', $post->id), $params);
+        $response->assertInvalid(['title', 'short_description', 'full_details']);
+
+        $params = ['title' => 'test post is here'];
+        $response = $this->actingAs($user)->put(route('post.update', $post->id), $params);
+        $response->assertSessionHasErrors(['short_description', 'full_details']);
+
+        $params = ['title' => 'test title', 'short_description' => 'lorem ipsum dolor emet sit.'];
+        $response = $this->actingAs($user)->put(route('post.update', $post->id), $params);
+        $response->assertSessionHasErrors(['full_details']);
+
+        $params = ['title' => 'test title', 'short_description' => 'lorem ipsum dolor emet sit.', 'full_details' => 'ha ha ha lorem ipsum dolor emit sit'];
+        $response = $this->actingAs($user)->put(route('post.update', $post->id), $params);
+        $response->assertSessionHasNoErrors();
+
+        $params = [
+            'title' => 'test title',
+            'short_description' => 'lorem ipsum dolor emet sit.',
+            'full_details' => 'ha ha ha lorem ipsum dolor emit sit',
+            'image' => 'new UploadedFile(public_path(\'img/1.png\'), \'1.png\', null, null, true)'
+        ];
+        $response = $this->actingAs($user)->put(route('post.update', $post->id), $params);
+        $response->assertSessionHasErrors(['image']);
+
+        $params = [
+            'title' => 'test title',
+            'short_description' => 'lorem ipsum dolor emet sit.',
+            'full_details' => 'ha ha ha lorem ipsum dolor emit sit',
+            'image' => new UploadedFile(public_path('img/1.png'), '1.png', null, null, true)
+        ];
+
+        $this->actingAs($user)
+            ->from(route('post.index'))
+            ->put(route('post.update', $post->id), $params)
+            ->assertStatus(302)
+            ->assertRedirect(route('post.index'));
+    }
+
+    public function test_post_delete()
+    {
+        $user = User::factory()->create();
+        Post::factory(10)->create();
+        $post = Post::query()->inRandomOrder()->first();
+
+        $this->actingAs($user)
+            ->delete(route('post.delete', $post->id))
+            ->assertOk();
+    }
+
+    public function test_delete_post_with_no_auth()
+    {
+        User::factory(10)->create();
+        Post::factory(10)->create();
+        $post = Post::query()->inRandomOrder()->first();
+
+        $this->delete(route('post.delete', $post->id))
+            ->assertStatus(302)
+            ->assertRedirect('login');
+    }
+
+    public function test_delete_post_with_no_auth_no_data()
+    {
+        $this->delete(route('post.delete', 10))
+            ->assertStatus(302)
+            ->assertRedirect('login');
+    }
+
+    public function test_delete_post_with_no_data()
+    {
+        $this->actingAs(User::factory()->create())
+            ->delete(route('post.delete', 10))
+            ->assertStatus(404);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,10 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function from(string $url)
+    {
+        $this->app['session']->setPreviousUrl($url);
+        return $this;
+    }
 }
